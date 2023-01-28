c44af355c1fe42b073845df4d00f7b2f5fc209d9
rajatxs <rxx175@gmail.com>
2023-01-28 15:18:47 +0530

    use ENABLE_LOGS

--- config/config.js
+++ config/config.js
@@ -2,11 +2,20 @@ import path from 'path';
 import { homedir } from 'os';
 import { getTag } from '../utils/common.js';
 
+/**
+ * Resolve boolean value from env var
+ * @param {string} envvar 
+ */
+function _resolveBoolean(envvar) {
+   return Boolean(Number(process.env[envvar]));
+}
+
 // global vars
 export const NODE_ENV = process.env.NODE_ENV;
 
 // common vars
 export const TAG = process.env.WIPE_TAG || getTag();
+export const ENABLE_LOGS = _resolveBoolean('WIPE_ENABLE_LOGS') || true;
 export const SESSION_ROOT = process.env.WIPE_SESSION_ROOT || path.join(homedir(), '.wipe');
 
 // SQLite config
